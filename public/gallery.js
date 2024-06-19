b82af811a4aee6f568719e128e91bab89772b1b8
Steve-vengence <stevevengence881@gmail.com>
2024-06-19 13:58:03 +0545

    adding sliding property in gallery

--- public/gallery.js
+++ public/gallery.js
@@ -1,6 +1,8 @@
 let full_img=document.querySelector(".full-img");
 let cntlr_img=document.querySelector(".cntrl-img");
 let close_btn=document.querySelector(".close-btn");
+let next_btn=document.querySelector(".next-btn");
+let prev_btn=document.querySelector(".prev-btn");
 let gal_img_nodelist=document.querySelectorAll(".gallery-img");
 const gal_img=Array.from(gal_img_nodelist);
 close_btn.addEventListener("click",()=>{
@@ -11,6 +13,21 @@ gal_img.forEach((val)=>{
         full_img.style.display= "Flex" ;
         let img=val.src;
         cntlr_img.src=img;
-
+        next_btn.addEventListener("click",()=>{
+            let img_no=img.slice(47,49);
+            let num=parseInt(img_no);
+            cntlr_img.src=`pic${num+1}.jpg`;
+            img=cntlr_img.src;
+        })
+        prev_btn.addEventListener("click",()=>{
+            let img_no=img.slice(47,49);
+            let num=parseInt(img_no);
+            if(num===10){
+                cntlr_img.src=`pic${num}.jpg`
+            }else{
+                cntlr_img.src=`pic${num-1}.jpg`
+            }
+            img=cntlr_img.src;
+        })
     })
 })
